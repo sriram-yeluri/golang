91e0f6f71069acf77f0447974da233f8e98cc4fd
Sriram Yeluri <yeluris@gmail.com>
2023-03-16 15:45:57 +0100

    Create conf.go

---
+++ pkg/conf.go
@@ -0,0 +1,15 @@
+package conf
+
+import "os"
+
+type Auth struct {
+	UserName string
+	Password string
+}
+
+func SetAuth() *Auth {
+	auth := new(Auth)
+	auth.UserName = os.Getenv("AUTH_USERNAME")
+	auth.Password = os.Getenv("AUTH_PASSWORD")
+	return auth
+}
